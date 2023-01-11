618ee4d8bf1e41990fe4a33878141f0d9edce896
damilolaakinbi <akinbidamilola074@gmail.com>
2023-01-11 16:37:43 +0100

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = { name: "Natasha", location: "Zambia" };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let employeeCopy = { ...employee };
+  employeeCopy[key] = value;
+  return employeeCopy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let employeeCopy = { ...employee };
+  delete employeeCopy[key];
+  return employeeCopy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
